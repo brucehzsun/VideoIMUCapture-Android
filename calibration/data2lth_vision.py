cd14f048dd37facd80eb21efdeab3561e6c0730f
hongzhi.sun <hongzhi.sun@rokid.com>
2023-02-06 19:35:02 +0800

    protobuf to csv

--- calibration/data2lth_vision.py
+++ calibration/data2lth_vision.py
@@ -3,20 +3,11 @@ import argparse
 from recording_pb2 import VideoCaptureData
 import os.path as osp
 import os
-import cv2
 import csv
-import yaml
-from pyquaternion import Quaternion
-import numpy as np
 import shutil
-from utils import OpenCVDumper
-
-
-
 
 def convert_to_lth(proto, result_path):
-
-    video_dir = osp.join(result_dir, 'video')
+    video_dir = osp.join(result_path, 'video')
     try:
         os.mkdir(video_dir)
     except OSError:
@@ -24,74 +15,74 @@ def convert_to_lth(proto, result_path):
 
     # Extract video timestamps
     frame_list = []
-    for i,frame_data in enumerate(proto.video_meta):
-        frame_list.append({
-            'frame': float(i),
-            'time': float(frame_data.time_ns)
-        })
+    for i, frame_data in enumerate(proto.video_meta):
+        frame_list.append([float(i), float(frame_data.time_ns)])
 
-    with open(osp.join(video_dir, 'video.data'), 'w') as f:
-        yaml.dump({'video': frame_list}, f, Dumper=OpenCVDumper)
+    with open(osp.join(video_dir, 'video.csv'), 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["#frame", "time"])
+        writer.writerows(frame_list)
 
     # Extract IMU data
     imu_list = []
-    for i,frame_data in enumerate(proto.imu):
-        imu_list.append({
-            'time': float(frame_data.time_ns),
-            'accel': list(frame_data.accel),
-            'gyro': list(frame_data.gyro),
-            'synced': 0.0,
-            'time_synced':0.0
-        })
+    for i, frame_data in enumerate(proto.imu):
+        imu_list.append([
+            float(frame_data.time_ns),
+            frame_data.accel[0],
+            frame_data.accel[1],
+            frame_data.accel[2],
+            frame_data.gyro[0],
+            frame_data.gyro[1],
+            frame_data.gyro[2],
+        ])
 
-    with open(osp.join(video_dir, 'imu.data'), 'w') as f:
-        yaml.dump({'imu': imu_list}, f, Dumper=OpenCVDumper)
+        with open(osp.join(video_dir, 'imu.csv'), 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["#time", "accel_x", "accel_y", "accel_z", "gyro_x", "gyro_y", "gyro_z"])
+            writer.writerows(imu_list)
 
-    # Write sync
-    with open(osp.join(video_dir, 'sync.data'), 'w') as f:
-        yaml.dump({'sync': [{'delay': 0.0}]}, f, Dumper=OpenCVDumper)
+        # Write sync
+        # with open(osp.join(video_dir, 'sync.data'), 'w') as f:
+        #     yaml.dump({'sync': [{'delay': 0.0}]}, f, Dumper=OpenCVDumper)
 
+    def copy_video(data_path, result_dir):
+        shutil.copyfile(osp.join(data_path, 'video_recording.mp4'),
+                        osp.join(result_dir, 'video', 'video.mp4'))
 
+    # def copy_calib(kalibr_path, result_dir):
+    #     with open(kalibr_path, 'r') as f:
+    #         calibration_dict = yaml.safe_load(f)
+    #
+    #     calib_dir = osp.join(result_dir, 'calibration')
+    #     try:
+    #         os.mkdir(calib_dir)
+    #     except OSError:
+    #         pass
+    #
+    #     with open(osp.join(calib_dir, 'calibration.yaml'), 'w') as f:
+    #         yaml.dump(calibration_dict, f, Dumper=OpenCVDumper)
 
-def copy_video(data_path, result_dir):
-    shutil.copyfile(osp.join(data_path, 'video_recording.mp4'),
-                    osp.join(result_dir, 'video', 'video.mp4'))
+    if __name__ == "__main__":
 
-def copy_calib(kalibr_path, result_dir):
-    with open(kalibr_path, 'r') as f:
-        calibration_dict = yaml.safe_load(f)
+        parser = argparse.ArgumentParser(description='Prepare video and proto for Kalibr')
+        parser.add_argument('data_dir', type=str, help='Path to folder with video_recording.mp4 and video_meta.pb3')
+        parser.add_argument('--result-dir', type=str, help='Path to result folder, default same as proto', default=None)
+        parser.add_argument('--kalibr', type=str, help='Path to Kalibr calibration file', default=None)
 
-    calib_dir = osp.join(result_dir, 'calibration')
-    try:
-        os.mkdir(calib_dir)
-    except OSError:
-        pass
+        args = parser.parse_args()
+        result_dir = args.result_dir if args.result_dir else osp.join(args.data_dir, 'lth')
+        try:
+            os.mkdir(result_dir)
+        except OSError:
+            pass
 
-    with open(osp.join(calib_dir, 'calibration.yaml'), 'w') as f:
-        yaml.dump(calibration_dict, f, Dumper=OpenCVDumper)
+        # Read proto
+        proto_path = osp.join(args.data_dir, 'video_meta.pb3')
+        with open(proto_path, 'rb') as f:
+            proto = VideoCaptureData.FromString(f.read())
 
-if __name__ == "__main__":
+        convert_to_lth(proto, result_dir)
+        copy_video(args.data_dir, result_dir)
 
-    parser = argparse.ArgumentParser(description='Prepare video and proto for Kalibr')
-    parser.add_argument('data_dir', type=str, help='Path to folder with video_recording.mp4 and video_meta.pb3')
-    parser.add_argument('--result-dir', type=str, help='Path to result folder, default same as proto', default = None)
-    parser.add_argument('--kalibr', type=str, help='Path to Kalibr calibration file', default = None)
-
-
-    args = parser.parse_args()
-    result_dir = args.result_dir if args.result_dir else osp.join(args.data_dir, 'lth')
-    try:
-        os.mkdir(result_dir)
-    except OSError:
-        pass
-
-    # Read proto
-    proto_path = osp.join(args.data_dir, 'video_meta.pb3')
-    with open(proto_path,'rb') as f:
-        proto = VideoCaptureData.FromString(f.read())
-
-    convert_to_lth(proto, result_dir)
-    copy_video(args.data_dir, result_dir)
-
-    if args.kalibr:
-        copy_calib(args.kalibr, result_dir)
+        # if args.kalibr:
+        #     copy_calib(args.kalibr, result_dir)
